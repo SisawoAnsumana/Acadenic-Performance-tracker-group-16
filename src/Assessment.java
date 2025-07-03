494674a13444a2ceb0c4609bb626a68c5b19dbec
pajohngomez <johnpagomez2@GMAIL.COM>
2025-07-03 08:32:28 -0700

    I implemented the Assestment1 and Assesment3 clss

---
+++ src/Assessment.java
@@ -0,0 +1,19 @@
+package src;
+
+class Assessment{
+    String course;
+    double weight;
+    double score;
+    double marks;
+    //double grade;
+    public Assessment(double weight,double score,String course,double marks){
+        this.weight = weight;
+        this.score = score;
+        this.marks=marks;
+        // this.grade = grade;
+        this.course = course;
+
+
+    }
+}
+
---
+++ src/Assessment3.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Assessment3{
+    String course;
+    double score;
+    double weight;
+
+    public Assessment3(String course,double score,double weight){
+        this.course = course;
+        this.score = score;
+        this.weight = weight;
+    }
+}
